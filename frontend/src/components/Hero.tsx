40f96cba8513f502d016c83f3180357ee3bbbbe7
otienojoseph <otienoj.cruz@gmail.com>
2024-12-26 23:31:22 +0300

    Add frontend base config

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div className="bg-blue-200">
+      <div className="container mx-auto">
+        <h1 className="font-bold text-3xl">Welcome to Compare Banks</h1>
+      </div>
+    </div>
+  );
+}
