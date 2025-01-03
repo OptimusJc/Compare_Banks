df6d065c5e94be0a4edcf54838cf4fe7cfc09c4a
jayudoye <joeludoye@gmail.com>
2025-01-03 17:16:26 +0100

    Home Component- Section 7 add

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -153,6 +153,36 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* section 6 */}
+      <section></section>
+
+      {/* section 7 */}
+      <section>
+        <div className='bg-blue-100'>
+          <div className='container mx-auto flex flex-col space-y-4 py-8 md:flex-row md:items-center md:justify-center md:px-2 lg:py-32'>
+            <div className='flex h-52 w-full items-center justify-center overflow-hidden md:h-60 md:w-1/2 lg:h-96 xl:w-[600px]'>
+              <img
+                src='https://placehold.co/1900x1280'
+                alt='Section 7 image'
+                className='w-full object-cover'
+              />
+            </div>
+            <div className='w-full text-balance p-2 px-8 md:w-1/2'>
+              <h1 className='text-4xl font-bold lg:text-6xl'>
+                Discover the Benefits of Comparing Banks with Compare Banks
+              </h1>
+              <p className='my-4 md:text-base lg:text-lg'>
+                Not sure how to compare bank packages? Get in touch with us! 
+                Our team is ready to assist you, even before you create an account.
+              </p>
+              <button className='my-4 rounded bg-blue-600 px-4 py-2 text-sm text-white lg:px-5 lg:py-4 lg:text-base'>
+                Start Comparing Now!
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
