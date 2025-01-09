7b9029841d3c7e14c5e0aa8e1bb2802bca04180a
otienojoseph <otienoj.cruz@gmail.com>
2025-01-10 00:51:29 +0300

    fix: Add correct api link

--- frontend/src/pages/Compare.tsx
+++ frontend/src/pages/Compare.tsx
@@ -30,7 +30,7 @@ export default function Compare() {
       try {
         setLoading(true);
 
-        const res = await fetch('http://127.0.0.1:5000/compare');
+        const res = await fetch('https://compare-banks.onrender.com/compare');
         if (!res.ok) {
           throw new Error('Failed to fetch data!');
         }
