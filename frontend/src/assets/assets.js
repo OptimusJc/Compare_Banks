c71d1b280d55353cf84596e621bcb54a4b62c5fc
jayudoye <joeludoye@gmail.com>
2025-01-09 21:05:31 +0100

    Assets fix

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,23 @@
-import heroImage1 from './heroImage1.png'
-import heroImage2 from './heroImage2.jpg'
-import heroImage3 from './heroImage3.jpg'
-import heroImage4 from './heroImage4.jpg'
+import heroImage1 from './heroImage1.png';
+import heroImage2 from './heroImage2.jpg';
+import heroImage3 from './heroImage3.jpg';
+import heroImage4 from './heroImage4.jpg';
+import about_1 from './about_1.jpeg';
+import about_2 from './about_2.jpeg';
+import Jay from './images/Jay.jpg';
+import Joseph from './images/Joseph.jpg';
+import MugishaDavid from './images/MugishaDavid.jpg';
+import Tshepiso from './images/Tshepiso.jpg';
 
 export const assets = {
     heroImage1,
     heroImage2,
     heroImage3,
     heroImage4,
+    about_1,
+    about_2,
+    Jay,
+    Joseph,
+    MugishaDavid,
+    Tshepiso
 }
