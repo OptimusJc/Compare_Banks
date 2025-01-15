a10a46a916006361352f17790214639a8678a62e
otienojoseph <otienoj.cruz@gmail.com>
2025-01-16 00:11:12 +0300

    fix: Add react-router and build

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Layout from '../components/Layout';
 import Compare from '../pages/Compare';
 import Home from '../pages/Home';
